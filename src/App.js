993037c73dd5665cf30485069caf088ca8c112ca
jreyes8631 <jreyes8631@gmail.com>
2021-03-14 16:58:33 -0400

    add tailwind to index.html

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import MoviesIndexContainer from './containers/MoviesIndexContainer';
+import MoviesFormContainer from './containers/MovieFormContainer';
 
 
 
 function App() {
   return (
     <div className="App">
-      <a>Hello</a>
+      <Router>
+        <Switch>
+          <Route exact path="/"> Movies</Route>
+          <Route path="/movies/new"> New Movie</Route>
+        </Switch>
+      </Router>
       
     </div>
   );
