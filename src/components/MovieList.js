43ca3264dc2b25f8fa9a88948470ff9548d5b303
jreyes8631 <jreyes8631@gmail.com>
2021-03-17 20:44:34 -0400

    add movielist to components

---
+++ src/components/MovieList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MovieList = ({movies}) => {
+    return (
+        <>
+        <ul>
+         <h1>Movie List</h1>
+        </ul>
+        </>
+    )
+        
+    
+}
+
+export default MovieList;
