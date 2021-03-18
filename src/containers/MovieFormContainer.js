c220556e740adf9b94da234d90917c0ad430a3c8
jreyes8631 <jreyes8631@gmail.com>
2021-03-17 20:46:35 -0400

    add form class

---
+++ src/containers/MovieFormContainer.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+
+export default class MovieFormContainer extends Component {
+    render(){
+        return (
+            <section className="max-w-6xl mx-auto mt-16">
+                Movie form goes here!
+            </section>
+        )
+    }
+}
