e524e2ad13d2a8dcdd6169ef8f3878e6d52a9baa
jreyes8631 <jreyes8631@gmail.com>
2021-03-23 23:17:02 -0400

    add link to movie for movie show page

--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MovieListItem = ({movie}) => {
- return <li className="" key={movie.id}>{movie.title}</li>;
+ return(
+    <li className="" key={movie.id}>
+        <Link to={`/movies/${movie.id}`}>{movie.title} </Link>
+    </li>
+ );
+        
 }
 
 export default MovieListItem;
