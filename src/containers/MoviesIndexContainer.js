ff8ef613c41ad5a21e3f2c13e669bf4ced569569
jreyes8631 <jreyes8631@gmail.com>
2021-03-25 07:56:24 -0400

    add a back button

--- src/containers/MoviesIndexContainer.js
+++ src/containers/MoviesIndexContainer.js
@@ -1,36 +1,43 @@
 import React, { Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchMovies} from '../actions/movies'
 import MovieList from '../components/MovieList';
 
-export default class MoviesIndexContainer extends Component {
+export class MoviesIndexContainer extends Component {
 
-    state = {
-     movies: [],
-     loading: true
-    }
+
 
     componentDidMount() {
-     fetch('http://localhost:3001/movies', { 
-         method: 'GET',
-         headers: {
-             'Accept': 'application/json',
-             'Content-Type': 'application/json'
-         }
-     })
-
-       .then(res => res.json())
-       .then(moviesJson => {
-        this.setState({
-            movies: moviesJson,
-            loading: false
-        })
-       })
+     this.props.DispatchfetchMovies();
     }
 
     render(){
+        if (this.props.loadingState === 'notStarted'){
+            return null
+        }
         return (
             <section className="max-w-6xl w-11/12 mx-auto mt-16">
-               { this.state.loading ? 'Loading content' : < MovieList movies={this.state.movies}/>}
+               { this.props.loadingState === 'inProgress' ? (
+                   'loading content') : (
+                   <MovieList movies={this.props.movies}/>
+                   
+                   )} 
             </section>
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+      movies: state.movies.movieList,
+      loadingState: state.movies.loadingState
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+      DispatchfetchMovies: () => dispatch(fetchMovies())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MoviesIndexContainer)
