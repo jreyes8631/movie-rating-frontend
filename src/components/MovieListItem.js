95c72f3c0b2b539cfd3c96e276c7ec45bcdc3c8a
jreyes8631 <jreyes8631@gmail.com>
2021-03-26 18:22:38 -0400

    add border to background of movie title

--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 const MovieListItem = ({movie}) => {
  return(
-    <li className="" key={movie.id}>
-        <Link to={`/movies/${movie.id}`}>{movie.title} </Link>
+    <li className="border-double border-4 border-light-blue-500 mt-4 shadow-lg px-4 py-6 mb-4 w-1/2" key={movie.id}>
+        <Link to={`/movies/${movie.id}`}><span>{movie.title}</span></Link>
     </li>
  );
         
