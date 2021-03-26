3a1ca8467ee4c504762eff05658a60195d50c84f
jreyes8631 <jreyes8631@gmail.com>
2021-03-26 18:24:34 -0400

    add reviewsReducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import moviesReducer from './movies';
+import reviewsReducer from './reviews';
 
 export default combineReducers({
     movies: moviesReducer,
+    reviews: reviewsReducer,
 })
 
