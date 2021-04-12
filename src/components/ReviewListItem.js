59495bdcbfa6881bf3bb2977af220fd8009a1060
jreyes8631 <jreyes8631@gmail.com>
2021-04-12 14:17:30 -0400

    add style to reviews index

--- src/components/ReviewListItem.js
+++ src/components/ReviewListItem.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const ReviewListItem = ({review}) => {
- return  <li className="" key={review.id}>{review.comment}</li>;
+ return  <li className="border-double border-4 border-light-blue-500 mt-4 shadow-lg px-4 py-6 mb-4 w-1/2" 
+ key={review.id}>{review.comment}</li>;
 }
 
 export default ReviewListItem;
